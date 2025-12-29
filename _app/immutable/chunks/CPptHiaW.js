422254bd736c2b063dec138903b2f98ac29ac9a6
YungSeepferd <YungSeepferd@users.noreply.github.com>
2025-12-29 09:40:33 +0000

    deploy: 4a121d422d1cee92b6e8e34586573ba40025c776

--- _app/immutable/chunks/CPptHiaW.js
+++
@@ -1,2 +0,0 @@
-const s=globalThis.__sveltekit_1es48hp?.base??"/BulgarianGermanLearningApp",a=globalThis.__sveltekit_1es48hp?.assets??s??"";export{a,s as b};
-//# sourceMappingURL=CPptHiaW.js.map
--- _app/immutable/entry/start.DJNMLH15.js
+++
@@ -1,2 +0,0 @@
-import{l as s,a}from"../chunks/Dp09BJYw.js";export{s as load_css,a as start};
-//# sourceMappingURL=start.DJNMLH15.js.map
