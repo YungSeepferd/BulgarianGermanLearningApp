c071c0667f22b73b78a38292360f288da99ffadf
YungSeepferd <YungSeepferd@users.noreply.github.com>
2026-04-20 06:33:08 +0000

    deploy: a6024449fc547341e12359c0d85fcfcb3ccb0a78

--- _app/immutable/chunks/BvoRipFN.js
+++
@@ -1,2 +0,0 @@
-const a=globalThis.__sveltekit_thuqk4?.base??"/BulgarianGermanLearningApp",s=globalThis.__sveltekit_thuqk4?.assets??a??"";export{s as a,a as b};
-//# sourceMappingURL=BvoRipFN.js.map
--- _app/immutable/entry/start.BHAoUCVy.js
+++
@@ -1,2 +0,0 @@
-import{l as s,a}from"../chunks/BLKQWJOM.js";export{s as load_css,a as start};
-//# sourceMappingURL=start.BHAoUCVy.js.map
